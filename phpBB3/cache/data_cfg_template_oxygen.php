d1d1a888a875fdb4cf03b2da77a4bbcd237e76f3
guochaiqi <guo3192@hotmail.com>
2014-04-22 22:38:55 -0400

    changed the all questions to link to forum

---
+++ phpBB3/cache/data_cfg_template_oxygen.php
@@ -0,0 +1,4 @@
+<?php exit; ?>
+1429755881
+235
+a:6:{s:4:"name";s:6:"Oxygen";s:9:"copyright";s:57:"&copy; <a href="http://www.bradleyv.com">Brad Veryard</a>";s:7:"version";s:3:"1.2";s:17:"template_bitfield";s:4:"lNg=";s:12:"inherit_from";s:9:"prosilver";s:8:"filetime";i:1386211644;}
---
+++ phpBB3/cache/sql_e35cbb64af30f1ffa31735725f41fe23.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+1398220481
+SELECT forum_id FROM p313_forums WHERE forum_options & 2 <> 0 LIMIT 1 OFFSET 0
+6
+a:0:{}
